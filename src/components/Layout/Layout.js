0ec65c8e45358e25a7b7ec657269bf46b5ee3d41
anujna.v <anujna.v@robosoftin.com>
2022-09-12 09:56:29 +0530

    Removed console log statements

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,6 @@ import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 import "./Layout.css";
 const Layout = (props) => {
-  console.log("PROPS", props.children);
   return (
     <div>
       <Header />
